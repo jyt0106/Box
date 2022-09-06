e86ebb2fc6a33b5cb45f5324fe8d232b40fffe77
jyt0106 <107200165+jyt0106@users.noreply.github.com>
2022-09-06 10:35:33 +0800

    Update SearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
@@ -1,9 +1,10 @@
 package com.github.tvbox.osc.ui.activity;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
