ae41d5f0af4cc845fccaaa11311e11283c377124
jyt0106 <107200165+jyt0106@users.noreply.github.com>
2022-09-10 20:08:47 +0800

    Update GridFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/GridFragment.java
@@ -205,10 +205,13 @@ public class GridFragment extends BaseLazyFragment {
                     else if(homeSourceBean.isQuickSearch() && Hawk.get(HawkConfig.FAST_SEARCH_MODE, false) && enableFastSearch()){
                         jumpActivity(FastSearchActivity.class, bundle);
                     }else{
-                        jumpActivity(DetailActivity.class, bundle);
+                        if(video.id.isEmpty() || video.id.startsWith("msearch:")){
+                            jumpActivity(SearchActivity.class, bundle);
+                        }else {
+                            jumpActivity(DetailActivity.class, bundle);
                     }
                  
-                      
+                      }  
                    
                 }
             }
